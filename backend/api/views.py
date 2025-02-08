7f36a032e4f099832bb46196222da083e982b012
Umair Maratab <p180045@nu.edu.pk>
2025-02-08 11:00:54 +0500

    Add view for user_registration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() # Check if user already exists.
+    serializer_class = User # user model
+    permission_classes = [AllowAny] # Anyone can register
