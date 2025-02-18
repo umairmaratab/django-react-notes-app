1ca4f9150dd67adf773ea495d89a10280aa5c2f4
Umair Maratab <p180045@nu.edu.pk>
2025-02-18 18:13:53 +0500

    Added routes for notes creation and deletion

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,7 +6,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes") # Access notes via User.notes
 
     def __str__(self):
         return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+# Creating Note view
+class NoteListCreate(generics.ListCreateAPIView): # List view will list notes user created or create new note
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated] # To call this route you should be authenticated (JWT)
+
+    # Get current user notes
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid(): # if note data is valid
+            serializer.save(author=self.request.user) # Add field manual as we set it manually
+        else:
+            print(serializer.errors)
+
+# Deleting Note view
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Get current user notes
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
